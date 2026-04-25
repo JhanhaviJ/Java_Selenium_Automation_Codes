8171038890f2629075b129da66fa4e279cce9750
JhanhaviJ <jhanhavij2003@gmail.com>
2026-04-25 11:09:30 +0530

    Update AlertPopUps.java

--- src/popups/AlertPopUps.java
+++ src/popups/AlertPopUps.java
@@ -1,4 +1,38 @@
 package popups;
+// Characteristics:
+
+// We can’t inspect this popup.
+// We can’t move this popup.
+// This popup will be present in the middle of the webpage under the URL.
+// We can’t perform any action on the webpage until we handle this popup.
+// This popup will have a message and an OK button (Alert popup).
+// If it has OK and Cancel buttons → (Confirmation popup).
+// If it has a textbox with OK and Cancel buttons → (Prompt popup).
+// 📌 Notes:
+
+// Since alert popup is created using JavaScript language, it is also called a JavaScript popup.
+// How do you handle Alert popup or JavaScript popup?
+
+// We can handle Alert popup by using:
+// driver.switchTo().alert() statement
+
+// What is the return type of alert() method?
+
+// The return type of alert() method is Alert interface.
+
+// Methods:
+
+// accept():
+// It is used to click on OK button.
+
+// dismiss():
+// It is used to click on Cancel button.
+
+// getText():
+// It is used to get the text from the alert popup.
+
+// sendKeys():
+// It is used to enter text into the alert popup (Prompt popup).
 
 import java.time.Duration;
 
